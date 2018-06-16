492ef64e47e16053cf48fbbdb77c01278462920f
Andrew <artas.96@mail.ru>
2018-06-16 17:13:21 +0400

    Add toolbar

--- app/src/main/java/com/example/a65apps/daggerlesson/presentation/common/BaseToolbarFragment.java
+++ app/src/main/java/com/example/a65apps/daggerlesson/presentation/common/BaseToolbarFragment.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.a65apps.daggerlesson.R;
@@ -29,6 +28,7 @@ public abstract class BaseToolbarFragment extends BaseFragment implements BackPr
                 .setDisplayHomeAsUpEnabled(true);
         appCompatActivity.getSupportActionBar()
                 .setDisplayHomeAsUpEnabled(true);
+        appCompatActivity.setTitle("Blablabla");
     }
 
 
